d3aa5859622cd1eeac140554c0b60c89e2a71ee4
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-26 20:09:58 +0900

    [feat] project-member 모듈 생성

---
+++ OmokHub/src/main/java/org/omoknoone/omokhub/projectmember/ProjectMember.java
@@ -0,0 +1,44 @@
+package org.omoknoone.omokhub.projectmember;
+
+public class ProjectMember {
+    private int projectMemberId;
+    private String userId;
+    private int projectTeamId;
+    private String memberCategory;
+
+    public ProjectMember() {
+    }
+
+    public ProjectMember(int projectMemberId, String userId, int projectTeamId, String memberCategory) {
+        this.projectMemberId = projectMemberId;
+        this.userId = userId;
+        this.projectTeamId = projectTeamId;
+        this.memberCategory = memberCategory;
+    }
+
+    public int getProjectMemberId() {
+        return projectMemberId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public int getProjectTeamId() {
+        return projectTeamId;
+    }
+
+    public String getMemberCategory() {
+        return memberCategory;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectMember{" +
+                "projectMemberId=" + projectMemberId +
+                ", userId='" + userId + '\'' +
+                ", projectTeamId=" + projectTeamId +
+                ", memberCategory='" + memberCategory + '\'' +
+                '}';
+    }
+}
