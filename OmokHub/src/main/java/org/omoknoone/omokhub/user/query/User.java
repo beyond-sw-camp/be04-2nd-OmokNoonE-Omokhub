50b9d82810d8ad0fc544d132a341de07d86f3440
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-26 19:29:47 +0900

    [feat] User 엔티티 생성

--- OmokHub/src/main/java/org/omoknoone/omokhub/user/query/User.java
+++ OmokHub/src/main/java/org/omoknoone/omokhub/user/query/User.java
@@ -1,4 +1,17 @@
 package org.omoknoone.omokhub.user.query;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class User {
+    private String userId;
+    private String name;
+    private String nickname;
+    private String password;
+    private String email;
+    private String phoneNum;
+    private String address;
+    private LocalDate birthday;
+    private LocalTime signUpDate;
+    private boolean isWithdraw;
 }
