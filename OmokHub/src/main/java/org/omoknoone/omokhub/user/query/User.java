29f22da1f49619b5e41fd35c7eff180ebd147873
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-26 19:32:01 +0900

    [feat] User 생성자, Getter, toString 메소드 추가

--- OmokHub/src/main/java/org/omoknoone/omokhub/user/query/User.java
+++ OmokHub/src/main/java/org/omoknoone/omokhub/user/query/User.java
@@ -14,4 +14,76 @@ public class User {
     private LocalDate birthday;
     private LocalTime signUpDate;
     private boolean isWithdraw;
+
+    public User() {
+    }
+
+    public User(String userId, String name, String nickname, String password, String email, String phoneNum, String address, LocalDate birthday, LocalTime signUpDate, boolean isWithdraw) {
+        this.userId = userId;
+        this.name = name;
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.address = address;
+        this.birthday = birthday;
+        this.signUpDate = signUpDate;
+        this.isWithdraw = isWithdraw;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public LocalTime getSignUpDate() {
+        return signUpDate;
+    }
+
+    public boolean isWithdraw() {
+        return isWithdraw;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                ", address='" + address + '\'' +
+                ", birthday=" + birthday +
+                ", signUpDate=" + signUpDate +
+                ", isWithdraw=" + isWithdraw +
+                '}';
+    }
 }
