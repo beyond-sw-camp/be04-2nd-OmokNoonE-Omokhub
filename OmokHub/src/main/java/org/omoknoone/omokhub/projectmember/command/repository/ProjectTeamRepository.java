eaa6004b2fa0e54cc2a967b455977821cb5c263a
JaeWon Lee <jlee38266@gmail.com>
2024-03-06 18:09:14 +0900

    [feat] projectmember jpa 프로젝트 팀 생성 & 삭제 기능  구현
    JPA를 적용한 프로젝트 팀 생성과 삭제 기능을 구현했습니다.

--- OmokHub/src/main/java/org/omoknoone/omokhub/projectmember/command/repository/ProjectTeamRepository.java
+++ OmokHub/src/main/java/org/omoknoone/omokhub/projectmember/command/repository/ProjectTeamRepository.java
@@ -1,4 +1,7 @@
 package org.omoknoone.omokhub.projectmember.command.repository;
 
-public interface ProjectTeamRepository {
+import org.omoknoone.omokhub.projectmember.command.aggregate.ProjectTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectTeamRepository extends JpaRepository<ProjectTeam, Integer> {
 }
