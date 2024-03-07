5e6affe9c2ebc28f7f4c5bbfdc186ed77a8a7e4b
Jongchan_Choi <118791747+CJC0512@users.noreply.github.com>
2024-03-07 20:28:48 +0900

    Update SeekingMemberPostServiceImpl.java
    logger 제거

--- project-service/src/main/java/org/omoknoone/omokhub/seekingmemberpost/query/service/SeekingMemberPostServiceImpl.java
+++ project-service/src/main/java/org/omoknoone/omokhub/seekingmemberpost/query/service/SeekingMemberPostServiceImpl.java
@@ -1,24 +1,18 @@
 package org.omoknoone.omokhub.seekingmemberpost.query.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.omoknoone.omokhub.seekingmemberpost.query.dto.SeekingMemberPostCriteriaDTO;
 import org.omoknoone.omokhub.seekingmemberpost.query.dto.SeekingMemberPostDTO;
 import org.omoknoone.omokhub.seekingmemberpost.query.entity.SeekingMemberPost;
 import org.omoknoone.omokhub.seekingmemberpost.query.repository.SeekingMemberPostMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("QuerySeekingMemberPostService")
-@Slf4j
 public class SeekingMemberPostServiceImpl implements SeekingMemberPostService {
 
     private final SeekingMemberPostMapper seekingMemberPostMapper;
-
-    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     public SeekingMemberPostServiceImpl(SeekingMemberPostMapper seekingMemberPostMapper) {
@@ -29,10 +23,8 @@ public class SeekingMemberPostServiceImpl implements SeekingMemberPostService {
     public List<SeekingMemberPostDTO> searchSeekingMemberPostByCriteria(
             SeekingMemberPostCriteriaDTO seekingMemberPostCriteriaDTO) {
 
-        logger.info("service impl Mapper이전 : {}", seekingMemberPostCriteriaDTO);
         List<SeekingMemberPostDTO> seekingMemberPostList =
                 seekingMemberPostMapper.selectSeekingMemberPostList(seekingMemberPostCriteriaDTO);
-        logger.info("service impl Mapper이후 : {}", seekingMemberPostList);
 
         return seekingMemberPostList;
     }
