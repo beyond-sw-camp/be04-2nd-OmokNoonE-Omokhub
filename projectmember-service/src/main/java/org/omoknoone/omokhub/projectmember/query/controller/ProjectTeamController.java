f4dabad65dfd1b17f950dc4576397cdc0986e9ac
JaeWon Lee <jlee38266@gmail.com>
2024-03-07 11:20:08 +0900

    [feat] projectmember-service 작업용 브랜치 생성

--- projectmember-service/src/main/java/org/omoknoone/omokhub/projectmember/query/controller/ProjectTeamController.java
+++ projectmember-service/src/main/java/org/omoknoone/omokhub/projectmember/query/controller/ProjectTeamController.java
@@ -1,4 +1,7 @@
 package org.omoknoone.omokhub.projectmember.query.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("queryProjectTeamController")
 public class ProjectTeamController {
 }
