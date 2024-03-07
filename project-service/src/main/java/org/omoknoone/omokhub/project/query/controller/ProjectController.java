7b93e3c799952c74c87f6f84e41e7660ce8b9adb
Jongchan_Choi <118791747+CJC0512@users.noreply.github.com>
2024-03-07 16:52:54 +0900

    Update ProjectController.java
    사용하지 않는 import 삭제 및 로그 작성 삭제

--- project-service/src/main/java/org/omoknoone/omokhub/project/query/controller/ProjectController.java
+++ project-service/src/main/java/org/omoknoone/omokhub/project/query/controller/ProjectController.java
@@ -1,13 +1,9 @@
 package org.omoknoone.omokhub.project.query.controller;
 
-import jakarta.ws.rs.Path;
-import lombok.extern.slf4j.Slf4j;
 import org.omoknoone.omokhub.project.query.dto.ProjectAndScheduleDTO;
 import org.omoknoone.omokhub.project.query.dto.ProjectAndTeamAndMemberDTO;
 import org.omoknoone.omokhub.project.query.dto.ProjectDTO;
 import org.omoknoone.omokhub.project.query.service.ProjectService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +17,9 @@ import java.util.List;
 
 @RestController("QueryProjectController")
 @RequestMapping("/project/projects")
-//@Slf4j
 public class ProjectController {
     private final ProjectService projectService;
-    Logger logger = LoggerFactory.getLogger(getClass());
+    
     @Autowired
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
@@ -41,11 +36,13 @@ public class ProjectController {
         List<ProjectAndTeamAndMemberDTO> projectAndTeamAndMemberDTOList = projectService.searchProjectByStatus(status);
         return ResponseEntity.status(HttpStatus.OK).body(projectAndTeamAndMemberDTOList);
     }
+    
     @GetMapping("/team-name/{teamName}")
     public ResponseEntity<List<ProjectAndTeamAndMemberDTO>> getProjectByTeam(@PathVariable("teamName") String teamName){
         List<ProjectAndTeamAndMemberDTO> projectAndTeamAndMemberDTOList = projectService.searchProjectByTeamName(teamName);
         return ResponseEntity.status(HttpStatus.OK).body(projectAndTeamAndMemberDTOList);
     }
+    
     @GetMapping("/member-id/{memberId}")
     public ResponseEntity<List<ProjectAndTeamAndMemberDTO>> getProjectByMember(@PathVariable("memberId") String memberId){
         List<ProjectAndTeamAndMemberDTO> projectAndTeamAndMemberDTOList = projectService.searchMyProjects(memberId);
