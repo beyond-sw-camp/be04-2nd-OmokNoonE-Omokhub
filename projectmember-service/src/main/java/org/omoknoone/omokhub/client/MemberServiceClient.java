3da80c6c68efaa9bb3bf48a80dbc5435b1754010
orlzlL <yerincho06@gmail.com>
2024-03-08 15:53:05 +0900

    [feat] 중간 객체 생성을 위한 setup파일 업로드
    client 디렉토리, ResponseMemberDTO 추가됨

--- projectmember-service/src/main/java/org/omoknoone/omokhub/client/MemberServiceClient.java
+++ projectmember-service/src/main/java/org/omoknoone/omokhub/client/MemberServiceClient.java
@@ -1,7 +1,18 @@
 package org.omoknoone.omokhub.client;
-import org.omoknoone.omokhub.projectmember.command.vo.ResponseMember;
 
+import org.omoknoone.omokhub.projectmember.query.dto.ResponseMemberDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+/* 메모. 서비스 이름 및 url 맞추어서 수정 */
+@FeignClient(name = "omok-user-service", url = "localhost:58900")
 public interface MemberServiceClient {
-    ResponseMember getMemberById(String memberId);
+
+    /* 메모. 민석님께 받으면 맞추어서 수정 */
+    @GetMapping("/<gateway 경로 적기>></{memberId}")
+    List<ResponseMemberDTO> getMemberById(@PathVariable("memberId") String memberId);
 
 }
