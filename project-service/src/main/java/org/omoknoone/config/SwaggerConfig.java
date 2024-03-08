cac6d5c90448bc66993a664ecdfcc2acec69a058
jihyeongeun <hyeongeun0924@gmail.com>
2024-03-08 11:43:10 +0900

    SwaggerConfig 파일 추가
    SwaggerConfig 파일 추가

---
+++ project-service/src/main/java/org/omoknoone/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package org.omoknoone.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+/* 설명. http://localhost:58000/swagger-ui/index.html */
+@OpenAPIDefinition(
+        info = @Info(title = "Omok API 명세서",
+                description = "Omok project API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+}
