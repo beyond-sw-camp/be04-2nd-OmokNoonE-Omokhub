1e056161b4109fdd3cf43bad8f61b870d90707c3
CJC0512 <ehdchs2006@gmail.com>
2024-03-09 21:23:18 +0900

    [fix] user-service application fix

--- user-service/src/main/java/org/omoknoone/omokhub/OmokHubApplication.java
+++ user-service/src/main/java/org/omoknoone/omokhub/OmokHubApplication.java
@@ -4,10 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class OmokHubApplication {
 
     public static void main(String[] args) {
@@ -18,4 +22,7 @@ public class OmokHubApplication {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){ return new BCryptPasswordEncoder();}
 }
