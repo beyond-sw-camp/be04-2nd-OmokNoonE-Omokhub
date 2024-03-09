418cdde3f804dbf5c3080c68f2933e8bac009d50
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-03-09 21:30:06 +0900

    [fix[ 병합 충돌 해결

--- user-service/src/main/java/org/omoknoone/omokhub/OmokHubApplication.java
+++ user-service/src/main/java/org/omoknoone/omokhub/OmokHubApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
