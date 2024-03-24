c0e752eda1a12ddea4e5d242c56bc9f577329e41
JaeWon Lee <jlee38266@gmail.com>
2024-03-25 08:25:16 +0900

    [feat] project-team exceptions 별도의 클래스 및 예외 메세지를 작성할 수 있는 기능 구현
    향후 예외 처리 관련된 메세지 출력을 전역적으로 관리하기 위해서 클래스를 따로 구분하여 기능을 구현하였습니다.

---
+++ projectmember-service/src/main/java/org/omoknoone/omokhub/exceptions/BusinessRuleViolationException.java
@@ -0,0 +1,8 @@
+package org.omoknoone.omokhub.exceptions;
+
+public class BusinessRuleViolationException extends RuntimeException {
+    public BusinessRuleViolationException(String message) {
+        super(message);
+    }
+}
+
