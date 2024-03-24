535f08fc6bec59b3c4193c67d0511801a268592a
JaeWon Lee <jlee38266@gmail.com>
2024-03-25 08:21:14 +0900

    [feat] project-team projectTeam 테스트 코드 유효성 검사를 위해 projectTeamSerivceImpl에 유효성 검증 기능 추가
    테스트 코드에서 예외처리 및 특정 조건을 만족하는 상황을 테스트 하기 위한 기능을 구현하였습니다.

--- projectmember-service/src/main/java/org/omoknoone/omokhub/projectmember/command/service/ProjectTeamServiceImpl.java
+++ projectmember-service/src/main/java/org/omoknoone/omokhub/projectmember/command/service/ProjectTeamServiceImpl.java
@@ -1,6 +1,7 @@
 package org.omoknoone.omokhub.projectmember.command.service;
 
 import org.modelmapper.ModelMapper;
+import org.omoknoone.omokhub.exceptions.BusinessRuleViolationException;
 import org.omoknoone.omokhub.projectmember.command.aggregate.ProjectTeam;
 import org.omoknoone.omokhub.projectmember.command.dto.ProjectTeamDTO;
 import org.omoknoone.omokhub.projectmember.command.repository.ProjectTeamRepository;
@@ -30,6 +31,26 @@ public class ProjectTeamServiceImpl implements ProjectTeamService {
         ProjectTeam projectTeam = modelMapper.map(projectTeamDTO, ProjectTeam.class);
         projectTeamRepository.save(projectTeam);
         projectTeam.getAddedDate();
+
+        /* 유효성 검사 */
+        validateProjectTeamDTO(projectTeamDTO);
+    }
+
+    private void validateProjectTeamDTO(ProjectTeamDTO teamDTO) {
+        // maxMember가 0명 초과 10명 이하인지 검사
+        if (teamDTO.getMaxMember() <= 0 || teamDTO.getMaxMember() > 10) {
+            throw new IllegalArgumentException("최대 정원은 1명 이상 10명 이하이어야 합니다.");
+        }
+
+        /* 팀 이름이 null 또는 공백인지 검사*/
+        if (teamDTO.getTeamName() == null || teamDTO.getTeamName().trim().isEmpty()) {
+            throw new IllegalArgumentException("팀명은 공백 또는 띄어 쓰기만으로 작명할 수 없습니다.");
+        }
+
+        // currentMember가 maxMember를 초과하지 않는지 검사
+        if (teamDTO.getCurrentMember() > teamDTO.getMaxMember()) {
+            throw new BusinessRuleViolationException("현재 인원은 최대 인원을 초과할 수 없습니다.");
+        }
     }
 
     @Override
